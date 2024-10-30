c2fa56287b16d7e4db79c6ef8fb76ec2891ec403
kestsaurav21 <kestsaurav21@outlook.com>
2024-10-30 16:34:53 +0530

    commit

---
+++ SwapColor/index.js
@@ -0,0 +1,57 @@
+
+
+const createBox = () => {
+    let container = document.querySelector("#container");
+
+    let colors = [
+        "green",
+        "green",
+        "green",
+        "red",
+        "red",
+        "red",
+        "blue",
+        "blue",
+        "blue"
+    ]
+
+    colors.forEach((color) => {
+        let newBox = document.createElement("div");
+        newBox.style.background = color;
+        newBox.classList.add("box");
+        newBox.addEventListener("click", () => swapColor(newBox) )
+
+        container.appendChild(newBox);
+    })
+
+
+}
+
+createBox();
+
+let counter = 0;
+let boxOne = boxTwo = null;
+let colorOne = colorTwo = "";
+
+const swapColor = (box) => {
+
+    counter+=1;
+    if(counter === 1){
+        boxOne = box;
+        colorOne = box.style.background;
+    }
+    if(counter === 2){
+        boxTwo = box;
+        colorTwo = box.style.background;
+    }
+
+    //Swap color 
+    boxOne.style.background = colorTwo;
+    boxTwo.style.background = colorOne;
+
+    counter = 0;
+    boxOne = boxTwo = null;
+    colorOne = colorTwo = "";
+    
+}
+
